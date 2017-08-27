1e9268aedda52138473df97779ed6b88c3d7c3a5
Pierpaolo <plucarelli.94@gmail.com>
2017-08-27 23:11:17 +0100

    added index.html

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the forum index.")
+    return render(request, 'index.html', context=None)
