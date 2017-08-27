78b4b25eaaced91e4d9b80a2dc992be12c91796f
Pierpaolo <plucarelli.94@gmail.com>
2017-08-27 23:27:56 +0100

    sub templates dir

--- forum/views.py
+++ forum/views.py
@@ -5,4 +5,4 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', context=None)
+    return render(request, 'forum/index.html', context=None)
